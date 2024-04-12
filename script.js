93de635a457255865a3a78466df62c5bc019168d
varsha2k3 <166714937+varsha2k3@users.noreply.github.com>
2024-04-12 11:01:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function fetchAndDisplayQuestions() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    fetch(`https://varsha2k3.github.io/SatsangTechnologiesProject/api/questions?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`)
+    fetch(`/api/questions?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`)
       .then(response => response.json())
       .then(data => {
         const questions = data.questions;
