442be51169fec8e1f3ee1d1489500b17ae037209
varsha2k3 <166714937+varsha2k3@users.noreply.github.com>
2024-04-12 17:35:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
   function fetchAndDisplayQuestions() {
     const name = document.getElementById('name').value;
     const email = document.getElementById('email').value;
-    fetch(` /questions.json?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`)
+    fetch(`/api/questions.json?name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}`)
       .then(response => response.json())
       .then(data => {
         const questions = data.questions;
